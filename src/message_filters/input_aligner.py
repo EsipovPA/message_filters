9c72a237e39ac3652d18a2397da3f4c7f009820c
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-06-22 09:27:55 +0200

    Fixed merge
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- src/message_filters/input_aligner.py
+++ src/message_filters/input_aligner.py
@@ -28,12 +28,8 @@
 
 """Input aligner for synchronizing messages from multiple sources based on their timestamps."""
 
-<<<<<<< HEAD
-=======
 from __future__ import annotations
 
-from dataclasses import dataclass
->>>>>>> d1b4e46 (Defer annotation evaluation to fix RHEL import.)
 import heapq
 import threading
 import typing as tp
